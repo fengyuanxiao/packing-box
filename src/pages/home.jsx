8a183ad192bb6fc8a0e4481dae3c59f52b5e62b9
fyxiao <nbtv@foxmail.com>
2020-12-18 14:57:40 +0800

    更变home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,56 +6,56 @@ import * as dd from 'dingtalk-jsapi';
 
 import './home.css';
 
-let s = 0;  // 点击选择按钮，成本价格叠加值  
-// 储存select选中的值
-const arrs = [];
-const imgArrs = [];
-// state数据是不可变值，每次修改都应该返回一个新值，将其合并成一个新数组传给  setVal()
-const newArrs = [];
-const newImgArrs = [];
-// 选中按钮点击后 规格默认选中
-let a = [];
+const checkboxArrs = [];      //储存选择按钮选中的数据
+const newCheckboxArrs = [];   //合并已经储存的按钮选中的数组
+const moneyArrs = [];         //价格数组
+const newMoneyArr = [];       //合并后的价格数组
+
+let defaultValueArr = [];   // 点击选中按钮，默认选中数组
+let num = 0;
+
+// 成本添加
+let o = 0;
+
+// 判断是否选中重复
+const chongfuArr = [];
+
 
 function Home() {
 
+  // 储存的一级数据
+  const [oneData, setOneData] = useState(null);
+  // 储存获取到的二级分类数据
+  const [twoData, setTwoData] = useState(null);
+  // 储存获取到的三级分类数据
+  const [threeData, setThreeData] = useState(null);
 
-  // 主类
-  const [mainClass, setMainclass] = useState(null);
-  // 次类
-  // 包装盒名
-  const [titleArrs, setTitles] = useState(null);
-  // 包装盒详情
-  const [boxs, setBoxs] = useState(null);
-  const [titleName, setTitleName] = useState(null);
-  const [guiges, setGuiges] = useState(null);
+
+  // 储存选中包装盒的id
+  const [storageId, setStorageId] = useState(null);
+  // 图片
   const [boxImg, setBoxImg] = useState(null);
+  // 储存选中包装盒的价格
+  const [storageMoney, setStorageMoney] = useState(null);
 
-  const [subclass, setSubclass] = useState(null);
-  const [ifVals, setIfVals] = useState(null);
+  // 显示到多选框
+  const [twoValue, setTwoValue] = useState(null);
+  const [threeValue, setThreeValue] = useState(null);
+  // 合并多选框数组
+  const [concatArr, setConcatArr] = useState(null);
+  // 合并价格数组并保存到state
+  const [concatMoneyArr, setConcatMoneyArr] = useState(null);
 
-  const [guige, setGuige] = useState(null);
-  const [copys, setCopys] = useState(null);
+  // 没有选择规格，却点击选中按钮 判断
+  const [clockState, setClockState] = useState(false);
 
-  // 储存select值
-  const [vals, setVals] = useState(null);
-  const [imgArr, setImgArr] = useState(null);
-  // 判断 如果值是null 那么就不操作
-  const [news, setNews] = useState(null);
-  // 规格 点击选择按钮后默认选中
-  const [checkedList, setCheckedList] = useState(a);
-
-  const [onchangea, setOnchangea] = useState(false);
+  // 去重id
+  const [defaultValueArrs, setDefaultValueArrs] = useState(defaultValueArr);
 
   // 成本价
   const [costPrice, setCostPrice] = useState(0);
-
   // 包装方案名state
   const [inputVal, setInputVal] = useState('');
-
-  // 点击保存选择按钮 获取多选数据
-  const [checkedValue, setCheckedValues] = useState(null);
-  // 遮罩显示状态，默认为 false
-  const [isModalVisible, setIsModalVisible] = useState(false);
 
 
   const { Option } = Select;
@@ -81,8 +81,9 @@ function Home() {
     })
     .then(function (res) {
       if ( res.data.status ) {
-        // 获取主类数据
-        setMainclass(res.data.data);
+        // 储存获取的主类数据
+        setOneData(res.data.data);
+        // console.log(res.data.data);
 
       } else {
         message.warning(res.data.msg);
@@ -97,13 +98,8 @@ function Home() {
   },[])
   
 
-  // 分类盒 次类
+  // 一级分类盒回调函数 次类
   function handleChangeOne(value) {
-
-      // 包装盒名字
-      let titlesArr = [];
-      // 包装盒详情
-      let boxArr = [];
 
       console.log(value);
     // 获取次类
@@ -113,28 +109,10 @@ function Home() {
     })
     .then(function (res) {
       if ( res.data.status ) {
-        let datas = res.data.data;
-        // 获取次类数据
-        console.log(datas);
-        // 将其保存，渲染次类 select
-        setSubclass(Object.keys(datas));
-
-        setIfVals(datas);
         
-        // 循环最外层包装名称
-        for (const key in datas) {
-          let o = {};
-          
-          // console.log(key);
-          titlesArr.push(key);
-          o[key] = datas[key];
-          boxArr.push(o);
-        }
-        // 储存包装盒名字
-        setTitles(titlesArr);
-        // 储存包装盒详情
-        setBoxs(boxArr);
-        console.log(boxArr);
+        // 储存一级分类获取的二级分类数据
+        setTwoData(res.data.data);
+        console.log(res.data.data);
 
       } else {
         message.warning(res.data.msg);
@@ -145,155 +123,117 @@ function Home() {
     });
 
 
-    console.log('操作了第一个select');
-
   }
 
-  // 具体商品盒
+  // 二级分类，次类
   function handleChangeTwo(value) {
-    // console.log(value);
+    // console.log(`次类函数：${value}`);
+    // 储存二级分类选中的value
+    setTwoValue(value);
+    // 循环二级分类，得到键 值，判断二级分类函数回调的值是否等于当前值，则保存遍历三级分类
+    for (const key in twoData) {
+      if ( key === value ) {
+        setThreeData(twoData[key]);
+        console.log(twoData[key]);
 
-    for (let i = 0; i < titleArrs.length; i++) {
-      if ( titleArrs[i] === value ) {
-        let aa = boxs[i];
-        // console.log(aa);
-        for (const key in aa) {
-
-          let aBoxs = aa[key];
-          // console.log(aBoxs);
-          for (let j = 0; j < aBoxs.length; j++) {
-
-            a.push(`${value + aBoxs[j].规格 + "每" + aBoxs[j].单位 + aBoxs[j].单价}`);
-            // 包装盒图片
-            setBoxImg(aBoxs[j].img);
-            
-          }
-          // console.log(aBoxs[0].img);
-
-
-          // 包装盒名
-          setTitleName(value);
-          // 包装详情规格
-          setGuiges(aBoxs);
-        }
-      } else {
-        
       }
-      
     }
-    // console.log(titleArrs);
-
-    
-    // 第二个select选中的value
-    // console.log(value);
-
-
-    // 固定规格选中后面 显示名称
-    setGuige(value);
-    // setGuigeImg()
-    // console.log(ifValsArr);
-    // console.log(ifValsObj);
-
-    console.log('操作了第二个select');
+    console.log(defaultValueArr);
 
   }
 
-  // 规格select
+  // 三级分类，规格select
   function handleChangeThree(value) {
-    // console.log(value);
 
-    // 触发第三个select函数 将value值储存到news中
-    setNews(value);
-     // 规格select成功调用后才能触发选择按钮
-    setOnchangea(true);
-    console.log(arrs);
-    console.log(copys);
+    // 储存三级分类选中的value
+    setThreeValue(value);
     
-    for (let j = 0; j < arrs.length; j++) {
+    console.log(chongfuArr);
 
-      if ( arrs[j] === copys ) {
+    setClockState(true); //setClockState状态为false 无法选择规格
+
+    for (let j = 0; j < defaultValueArr.length; j++) {
+      
+      if ( chongfuArr[j] === value ) {
 
         message.warning('规格重复！');
-        // 规格select成功调用后才能触发选择按钮,如果有重复数据便禁止按钮
-        setOnchangea(false);
+        setClockState(false); //setClockState状态为false 无法选择规格
 
-      } else {
-
-        
+      }else {
+        console.log('继续');
       }
       
     }
-    console.log('操作了第三个select');
 
-  }
-  // 选择函数，选择将确定的规格存入数组中，后将遍历到eslect
-  function handleOk() {
+    // 遍历二级类目下的数据，获取三级类目的规格数据
+    for (let i = 0; i < threeData.length; i++) {
+      if ( threeData[i].规格 + threeData[i].单位 + threeData[i].单价 === value ) {
+        console.log(threeData[i].id);
+        // 储存规格id
+        setStorageId(threeData[i].id);
+        // 储存规格价格
+        setStorageMoney(threeData[i].单价);
 
-
-    if ( onchangea ) {
-
-      // guigeArrs.push(news);
-      console.log(titleName + news);
-      // 判断重复
-      setCopys(titleName + news);
-
-      // 图片数组
-      imgArrs.push(boxImg)
-      // 将包装名拼接到规格 再添加到数组中
-      arrs.push(titleName + news);
-  
-        // 合并新数组 arrs.concat(newArrs)///  并且去重
-        // setVal(Array.from(new Set(arrs.concat(newArrs))));
-        setVals(arrs.concat(newArrs));
-        setImgArr(imgArrs.concat(newImgArrs))
-        // console.log('操作了');
-
-        // 选择按钮 叠加成本价格
-        s += parseFloat(news.substring(news.length - 6));
-        // console.log(parseFloat(news.substring(news.length - 6)));
-        // console.log(s)
-        
-        // 显示成本价格
-        setCostPrice(s.toFixed(4));
-
-        // 最终需要循环出来的规格数据
-        // console.log(newObj);
-        // console.log(objs);
-        // console.log(arrs);
-        // console.log(s);
-  
-        // 更改点击状态
-        setOnchangea(false);
-      
-    } else {
-
-      message.warning('请选择规格！');
-      
+      }
     }
 
-    console.log('点击按钮了');
+  }
+  
+  // 选择函数，选择将确定的规格存入数组中，后将遍历到eslect
+  function handleOk() {
+    // console.log( twoValue + threeValue );
+    // console.log("点击了选择按钮" + storageId);
+
+    if ( clockState ) {
+
+      // console.log(threeValue);
+      chongfuArr.push(threeValue);
+      console.log(chongfuArr);
+
+      // 将选中的数据追加到数组
+      checkboxArrs.push(storageId + twoValue + threeValue);
+      // 合并数组
+      setConcatArr(checkboxArrs.concat(newCheckboxArrs));
+      
+      // 储存价格到数组中
+      moneyArrs.push(storageMoney);
+      // 合并价格数组 并保存
+      // setConcatMoneyArr(moneyArrs.concat(newMoneyArr));
+      console.log(moneyArrs);
+      console.log(parseFloat(storageMoney));
+      o += parseFloat(storageMoney)
+      setCostPrice(o.toFixed(4));
+
+      // 循环截取出包装盒id ，后选中按钮点击默认显示
+      defaultValueArr.push(checkboxArrs[num].slice(0,2));
+      num++;
+      console.log(defaultValueArr);
+
+      setClockState(false); //setClockState状态为false 无法选择规格
+
+    }else {
+      message.warning('请选择规格！');
+    }
+    
+
   }
 
   // 多选框
-  let cheSum = 0;
+  let sum = 0;
   function onChangea(checkedValues) {
 
-    // setAaState(true);
+    console.log(concatArr);
 
-    console.log('checked = ', checkedValues);
-    for (let i = 0; i < checkedValues.length; i++) {
-      
-      // console.log(checkedValues[i].substring(checkedValues[i].length - 6));
-      cheSum += parseFloat(checkedValues[i].substring(checkedValues[i].length - 6))
-      
-    }
-    setCostPrice(cheSum.toFixed(4));
+    // console.log(defaultValueArr);
+    // concatArr = checkedValues;
+    defaultValueArr = checkedValues;
+    // console.log(checkedValues);
+    console.log(defaultValueArr);
+    // o = sum;
 
-    //必要代码***** 更改为勾选中后的 成本总额
-    s = cheSum;
-
-    setCheckedValues(checkedValues);
-
+  }
+  function onCheckAllChange(params) {
+    
   }
 
   // 方案名 input回调取值
@@ -306,12 +246,10 @@ function Home() {
   // 保存按钮
   function handlePreservation() {
 
-    console.log(checkedValue)
     console.log(`方案包装名：${inputVal}`);
 
     if ( inputVal ) {
       // true 遮罩显示
-      setIsModalVisible(true);
 
     } else {
 
@@ -327,41 +265,40 @@ function Home() {
       <Spin spinning={false} tip="Loading...">
         <header className='header'>包装BOM</header>
         <div className='box'>
-          {/* 分类盒 */}
+          {/* 一级分类盒 */}
           <Select className="select_one" defaultValue="主类" style={{ width: 140 }} onChange={handleChangeOne}>
             {
-              mainClass ?
-                mainClass.map((tiem, index) => {
+              oneData ?
+                oneData.map((item, index) =>{
                   return(
-                    <Option key={index} value={tiem}>{tiem}</Option>
+                    <Option key={index} value={item}>{item}</Option>
                   )
                 })
               :
                 ''
             }
           </Select>
-          {/* 次类 具体商品盒 */}
+          {/* 二级分类,次类 具体商品盒 */}
           <Select defaultValue="次类" style={{ width: 220 }} onChange={handleChangeTwo}>
             {
-              titleArrs ?
-                titleArrs.map((tiem, index) => {
-                  return(
-                    <Option key={index} value={tiem}>{tiem}</Option>
-                  )
-                })
+              twoData ?
+              Object.keys(twoData).map((key, index) => {
+                return(
+                  <Option key={index} value={key}>{key}</Option>
+                )
+              })
               :
               ""
             }
-            {/* <Option value="孕妇内裤小飞机盒">孕妇内裤小飞机盒</Option> */}
           </Select>
-          {/* 规格 */}
+          {/* 三级分类,规格 */}
           <div className="specification">
             <Select className="select_three" defaultValue="规格" style={{ width: 280 }} onChange={handleChangeThree}>
               {
-                guiges ?
-                  guiges.map((item, index) => {
+                threeData ?
+                  threeData.map((item, index) => {
                     return(
-                      <Option key={index} value={item.规格 + "每" + item.单位 + item.单价}>{item.规格 + "每" + item.单位 + item.单价}</Option>
+                      <Option key={index} value={item.规格 + item.单位 + item.单价}>{item.规格 + item.单位 + item.单价}</Option>
                     )
                   })
                 :
@@ -372,29 +309,23 @@ function Home() {
           </div>
           {/* 多选框 */}
           <div className="specification_pull_down">
-            <Checkbox.Group defaultValue={checkedList} style={{ width: '100%' }} onChange={onChangea}>
+            {/* defaultValue={defaultValueArr} */}
+            <Checkbox.Group defaultValue={defaultValueArr} style={{ width: '100%' }} onChange={onChangea}>
               <Row>
                 {
-                  vals ?
-                    vals.map((item, index) => {
-                      // 截取得到图片链接
-                      // let httpImg = item.substring(item.search(/http/));
-                      // 去除图片链接
-                      // let a = item.split(httpImg);
-                        return(
-                          <Col data-a={index} key={index}>
-                              <Checkbox value={item} style={{ display: 'flex', alignItems: 'center' }}>
-                                <div className="guige">
-                                  <img src={imgArr[index]} alt="规格图"/>
-                                  <p>{item}</p>
-                                </div>
-                              </Checkbox>
-                          </Col>
-                        )
-                      })
-                      :
-                      ''
-                  }
+                  concatArr ?
+                    concatArr.map((item, index) => {
+                      console.log(defaultValueArr);
+                      console.log(item.slice(0,2));
+                      return(
+                        <Col key={index}>
+                          <Checkbox value={item.slice(0,2)}>{item.slice(2)}</Checkbox>
+                        </Col>
+                      )
+                    })
+                  :
+                    ""
+                }
               </Row>
             </Checkbox.Group>
           </div>
